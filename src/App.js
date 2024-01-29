d42a5555d2a80df16b8a0e374ffa6bc24a15fd7b
Rutik Kulkarni <86470947+RutikKulkarni@users.noreply.github.com>
2024-01-29 16:52:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,100 +1,51 @@
-import { useEffect, useState } from "react";
 
-function App() {
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const CountryFlags = () => {
   const [countries, setCountries] = useState([]);
-  const [showCountries, setShowCountries] = useState([]);
-  const [searchCountries, setSearchCountries] = useState("");
-  // const [specificCountries, setSpecificCountries] = useState([]);
-  // Fetch countries data on component mount
+  const [searchTerm, setSearchTerm] = useState('');
+
   useEffect(() => {
-    fetch("https://restcountries.com/v3.1/all")
-      .then((res) => res.json())
-      .then((data) => {
-        setCountries(data);
-        setShowCountries(data);
-      })
-      .catch((err) => console.error("Error fetching data: ", err));
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then((response) => setCountries(response.data))
+      .catch((error) => console.error('Error fetching data:', error));
   }, []);
-  // Filter countries based on search input
-  useEffect(() => {
-    let filteredData = countries.filter((country) =>
-      country.name.common.toLowerCase().includes(searchCountries.toLowerCase())
-    );
-    if(!searchCountries){
-      setShowCountries("")
-      setShowCountries(countries)
-    }else{
-      setShowCountries(filteredData)
-    }
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchCountries]);
-
-  function handleChange(e) {
-    setSearchCountries(e.target.value);
-  }
-
-  const cardStyle = {
-    width: "200px",
-    border: "1px solid #ccc",
-    borderRadius: "10px",
-    margin: "10px",
-    padding: "10px",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
   };
 
-  const imageStyle = {
-    width: "100px",
-    height: "100px",
-  };
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-  const countryCard = {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh",
-  };
-
-  const searchInputWrapper = {
-    width: "100%",
-    display: "flex",
-    justifyContent: "center",
-  };
-
-  const searchInput = {
-    marginTop: "5px",
-    height: "25px",
-    width: "50%",
-  };
   return (
     <div>
-      <div style={searchInputWrapper}>
+      <div className="navbar">
         <input
           type="text"
-          style={searchInput}
-          value={searchCountries}
-          placeholder="Search for countries..."
-          onChange={handleChange}
+          placeholder="Search for a country"
+          value={searchTerm}
+          onChange={handleSearch}
+          className="search-box"
         />
       </div>
-      <div style={countryCard}>
-        {showCountries.map((country) => (
-          <div key={country.cca3} style={cardStyle}>
+      <div className="country-cards">
+        {filteredCountries.map((country) => (
+          <div key={country.cca2} className="country-card">
             <img
-              style={imageStyle}
               src={country.flags.png}
               alt={`Flag of ${country.name.common}`}
             />
-            <h2>{country.name.common}</h2>
+            <p>{country.name.common}</p>
           </div>
         ))}
       </div>
     </div>
   );
-}
+};
 
-export default App;
+export default CountryFlags;
